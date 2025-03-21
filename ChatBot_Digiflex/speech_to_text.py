68f6e3d5ad782917899695c642b10ff4f9f8b8fa
Stuti0711 <stutinagaic07@gmail.com>
2025-03-21 16:15:47 +0530

    Bot

---
+++ ChatBot_Digiflex/speech_to_text.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+
+def recognize_speech():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        try:
+            audio = recognizer.listen(source, timeout=5)
+            text = recognizer.recognize_google(audio)
+            return text
+        except sr.UnknownValueError:
+            return "Could not understand audio."
+        except sr.RequestError:
+            return "Check your internet connection."
+
+if __name__ == "__main__":
+    print(recognize_speech())
